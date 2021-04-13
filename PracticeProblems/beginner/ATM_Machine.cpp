9d3f28e5c984102ba92f91e56f9016f932bfb711
Akash671 <akashsaini454545gmail.com>
2021-04-13 17:19:32 +0530

    ATM Machine Problems

--- PracticeProblems/beginner/ATM_Machine.cpp
+++ PracticeProblems/beginner/ATM_Machine.cpp
@@ -3,14 +3,14 @@
 #include<iostream>
 using namespace std;
 
-void solve(int n,int k,int a[])
+void solve(int NO_OF_PEOPLES,int ATM_AMOUNT,int PEOPLES_MONEY[])
 {
 	/* logic here */
-	for(int i=0;i<n;++i)
+	for(int i=0;i<NO_OF_PEOPLES;++i)
 	{
-		if(k>=a[i])
+		if(ATM_AMOUNT>=PEOPLES_MONEY[i])
 		{
-			k=k-a[i];
+			ATM_AMOUNT-=PEOPLES_MONEY[i];
 			cout<<1;
 		}
 		else
@@ -27,14 +27,14 @@ int main()
 	cin>>t;
 	while(t--)
 	{
-		int n,k;
-		cin>>n>>k;
-		int a[n];
-		for(int i=0;i<n;++i)
+		int NO_OF_PEOPLES,ATM_AMOUNT;
+		cin>>NO_OF_PEOPLES>>ATM_AMOUNT;
+		int PEOPLES_MONEY[NO_OF_PEOPLES];
+		for(int i=0;i<NO_OF_PEOPLES;++i)
 		{
-			cin>>a[i];
+			cin>>PEOPLES_MONEY[i];
 		}
-		solve(n,k,a);
+		solve(NO_OF_PEOPLES,ATM_AMOUNT,PEOPLES_MONEY);
 		cout<<endl;
 	}
 	return 0;
