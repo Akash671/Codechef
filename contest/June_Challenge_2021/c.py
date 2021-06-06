33a89ab114e0d8df10dce7a4ae5a8c3161ebbf9c
Akash671 <akashsaini454545gmail.com>
2021-06-06 15:00:45 +0530

    codechef june long challenge 2021 problem and solution

---
+++ contest/June_Challenge_2021/c.py
@@ -0,0 +1,16 @@
+"""
+author : @akash
+"""
+mod=1000000007
+def solve():
+  n,m=map(int,input().split())
+  #a=list(map(int,input().split()[:n])
+  if(n==1):
+     print(1)  
+  else:
+    x=(2**n-1)%mod
+    ans=x**m%mod
+    print(ans)
+
+for _ in range(int(input())):
+   solve()
